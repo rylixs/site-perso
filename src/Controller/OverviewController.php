d9ca5263ceaee268390e3c72287ab1df5a279390
rylix <charlesclimentpro@gmail.com>
2023-03-31 18:29:09 +0200

    feat(overview): ajout de la page de présentation

---
+++ src/Controller/OverviewController.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+final class OverviewController extends AbstractController
+{
+    #[Route('/', name: 'app_overview')]
+    public function index(): Response
+    {
+        return $this->render('pages/overview.html.twig');
+    }
+}
