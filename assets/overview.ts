a7c2d29e8d224337befe21daf89da5f9e77e2576
rylix <charlesclimentpro@gmail.com>
2023-03-31 22:32:52 +0200

    feat(overview): "à propos de moi"

--- assets/overview.ts
+++ assets/overview.ts
@@ -2,8 +2,27 @@ import './styles/overview.scss';
 
 import Parallax from 'parallax-js';
 
-const skybox = document.querySelector('#skybox') as HTMLElement | null;
+const getAge = (): string => {
+    const birthday = new Date('1994-01-24');
+    const now = new Date();
 
-if (skybox) {
-    new Parallax(skybox);
+    let age = now.getFullYear() - birthday.getFullYear();
+
+    if (
+        now.getMonth() < birthday.getMonth()
+        || (
+            now.getMonth() === birthday.getMonth()
+            && now.getDate() < birthday.getDate()
+        )
+    ) {
+        age--;
+    }
+
+    return age.toString();
 }
+
+const skybox = document.querySelector('#skybox') as HTMLElement | null;
+const age = document.querySelector('#age') as HTMLElement | null;
+
+if (skybox) new Parallax(skybox);
+if (age) age.innerText = getAge();
