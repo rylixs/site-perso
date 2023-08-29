708121dfda34d6abbfb9e65a8279292859150626
rylix <charlesclimentpro@gmail.com>
2023-08-29 22:37:26 +0200

    perf(cache): remplacement de cache validation par cache expiration

--- src/Controller/OverviewController.php
+++ src/Controller/OverviewController.php
@@ -4,14 +4,17 @@ declare(strict_types=1);
 
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Attribute\Cache;
 use Symfony\Component\Routing\Annotation\Route;
 
-final class OverviewController extends BaseController
+final class OverviewController extends AbstractController
 {
     #[Route('/', name: 'app_overview')]
-    public function index(): Response
+    #[Cache(maxage: 3600, public: true, mustRevalidate: true)]
+    public function __invoke(): Response
     {
-        return $this->cachableRender($this->render('pages/overview/index.html.twig'));
+        return $this->render('pages/overview/index.html.twig');
     }
 }
